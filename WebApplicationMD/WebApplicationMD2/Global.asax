9d398f4cb311ae120bbc2b93bc22db77e4ffa734
Mick <michael.deery@gmail.com>
2016-12-19 16:29:48 +1100

    2nd

---
+++ WebApplicationMD/WebApplicationMD2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationMD2.MvcApplication" Language="C#" %>
---
+++ WebApplicationMD/WebApplicationMD2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationMD2.Startup))]
+namespace WebApplicationMD2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
